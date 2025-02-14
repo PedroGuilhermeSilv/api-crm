e62a1591eb4063e3e3d0bb2b5eea6516a752c56e
guilherme <vinespedro55@gmail.com>
2025-02-14 00:32:19 -0300

    refactor: update storage repository connection method and remove .env file

--- src/core/storage/domain/repository.py
+++ src/core/storage/domain/repository.py
@@ -12,16 +12,8 @@ class StorageRepository(BaseModel):
     aws_secret_access_key: str = os.environ.get("AWS_SECRET_ACCESS_KEY")
     bucket_name: str = os.environ.get("BUCKET_NAME")
 
-    def connection(self) -> boto3.client:
-        return boto3.client(
-            "s3",
-            endpoint_url=self.endpoint_url.replace("https://", "http://"),
-            aws_access_key_id=self.aws_access_key_id,
-            aws_secret_access_key=self.aws_secret_access_key,
-            config=Config(s3={"addressing_style": "path"}, signature_version="s3v4"),
-            use_ssl=False,
-            region_name="us-east-1",
-        )
+    def connection(self):
+        raise NotImplementedError
 
     @abstractmethod
     def save_file(self, file: str) -> str:
--- src/core/storage/infra/tebi_io/domain/repository.py
+++ src/core/storage/infra/tebi_io/domain/repository.py
@@ -2,9 +2,22 @@ from src.core.storage.domain.repository import StorageRepository
 from src.core.product.domain.value_objects import UploadedFile
 import warnings
 import urllib3
+from botocore.config import Config
+import boto3
 
 
 class TebiIOStorageRepository(StorageRepository):
+
+    def connection(self):
+        return boto3.client(
+            "s3",
+            endpoint_url=self.endpoint_url.replace("https://", "http://"),
+            aws_access_key_id=self.aws_access_key_id,
+            aws_secret_access_key=self.aws_secret_access_key,
+            config=Config(s3={"addressing_style": "path"}, signature_version="s3v4"),
+            use_ssl=False,
+            region_name="us-east-1",
+        )
 
     def save_file(self, file: UploadedFile) -> str:
         # Suprime o aviso de SSL não verificado
